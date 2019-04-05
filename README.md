8c829ce8629caa1d3816e99008839aef22525b12
Jon <me@jontg.de>
2019-04-05 14:55:01 +0200

    Init Changes after Fork

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
-# quantum-battleship
-Battleship taking advantage of IBM's public facing 5 qubit quantum computer, following this tutorial (https://medium.com/@decodoku/how-to-program-a-quantum-computer-982a9329ed02) as a guide
+# Quantum Battleship
 
-## To Run
-- Install Jupyter (https://github.com/IBM/qiskit-sdk-py/blob/master/tutorial/sections/tutorial4developer.ipynb). Currently the program expects Jupyter to be installed in the same directory as this repo.
+A Battleship boardgame implementation using [IBM Qiskit](https://qiskit.org), following [this tutorial by decodoku](https://medium.com/@decodoku/how-to-program-a-quantum-computer-982a9329ed02), forked from [@RavenPillmann](https://github.com/RavenPillmann/quantum-battleship) and updated for the current Qiskit version.
+
+## How it works
+
+Run a battleship game on either a simulated or real quantum computer. If you want to run the game on a real quantum computer, you will have to obtain an IBM Q API token from [here](https://quantumexperience.ng.bluemix.net/qx/account/advanced) first.
+
+*more information soon.*
+
+## Run the Game
+
+tbd.
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import sys
 import getpass
 import math
-sys.path.append("../qiskit-sdk-py/")
-from qiskit import QuantumProgram
-import Qconfig
+from qiskit import IBMQ
+from qiskit import execute
+from qiskit import ClassicalRegister, QuantumRegister, QuantumCircuit
+from qiskit.providers.ibmq import least_busy
+from qiskit import BasicAer
 
 SHOTS = 1024
 
@@ -84,38 +86,43 @@ def playGame(device, shipPos):
         for player in range(2):
             print("Measuring damage to Player " + str(player + 1) + "...")
 
-            Q_SPECS = {
-                "circuits": [{
-                    "name": "gridScript",
-                    "quantum_registers": [{
-                        "name": "q",
-                        "size": 5
-                    }],
-                    "classical_registers": [{
-                        "name": "c",
-                        "size": 5
-                }]}],
-            }
+            # Q_SPECS = {
+            #     "circuits": [{
+            #         "name": "gridScript",
+            #         "quantum_registers": [{
+            #             "name": "q",
+            #             "size": 5
+            #         }],
+            #         "classical_registers": [{
+            #             "name": "c",
+            #             "size": 5
+            #     }]}],
+            # }
 
-            Q_program = QuantumProgram(specs=Q_SPECS)
-            gridScript = Q_program.get_circuit("gridScript")
-            q = Q_program.get_quantum_registers("q")
-            c = Q_program.get_classical_registers("c")
+            # Q_program = QuantumProgram(specs=Q_SPECS)
+            # gridScript = Q_program.get_circuit("gridScript")
+            # q = Q_program.get_quantum_registers("q")
+            # c = Q_program.get_classical_registers("c")
+
+            q = QuantumRegister(5)
+            c = ClassicalRegister(5)
+
+            circ = QuantumCircuit(q, c)
 
             for position in range(5):
                 for hit in range(bombPos[(player + 1) % 2][position]):
                     for ship in range(3):
                         if (position == shipPos[player][ship]):
                             frac = 1/(ship + 1)
-                            gridScript.u3(frac * math.pi, 0.0, 0.0, q[position])
+                            circ.u3(frac * math.pi, 0.0, 0.0, q[position])
 
             for qubit in range(5):
-                gridScript.measure(q[qubit], c[qubit])
+                circ.measure(q[qubit], c[qubit])
 
-            Q_program.set_api(Qconfig.APItoken, Qconfig.config["url"])
-            Q_program.execute(["gridScript"], device, SHOTS, wait=2, timeout=60)
+            # Q_program.set_api(Qconfig.APItoken, Qconfig.config["url"])
+            job = execute(circ, device, shots=SHOTS)
 
-            grid[player] = Q_program.get_counts("gridScript")
+            grid[player] = job.result().get_counts(circ)
 
         if (('Error' in grid[0].values()) or ('Error' in grid[1].values())):
             print("\nThe process timed out. Try this round again.\n")
@@ -140,9 +147,22 @@ def main():
     d = input("Do you want to play on a real device? (y/n)\n")
 
     if (d == "y"):
-        device = 'ibmqx2'
+        try:
+            IBMQ.load_accounts()
+        except:
+            token = input("Could not find IBM Q API token. Please generate one at https://quantumexperience.ng.bluemix.net/qx/account/advanced and paste it here: ")
+            try:
+                IBMQ.save_account(token)
+                IBMQ.load_accounts()
+            except:
+                print("Could not load API Token, please check your token again and rerun this program.")
+                exit()
+
+        device = least_busy(IBMQ.backends(simulator=False))
+
+        # device = 'ibmqx2'
     else:
-        device = 'local_qasm_simulator'
+        device = BasicAer.get_backend('qasm_simulator')
 
     # Read this as shipPos[player][ship] = position of player's ship
     shipPos = [
